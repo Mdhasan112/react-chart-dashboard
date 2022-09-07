e5c935c3e2fca4d12dc7b2b2f80d6f5dd77e3d3d
hasan <mdhasanhk.98@gmail.com>
2022-09-07 21:58:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 
 function App() {
+  const data = [
+    { name: 'Page A', uv: 200, pv: 2400, amt: 2400 },
+    { name: 'Page A', uv: 300, pv: 2400, amt: 2500 },
+    { name: 'Page A', uv: 400, pv: 2400, amt: 2600 },
+    { name: 'Page A', uv: 500, pv: 2400, amt: 2700 },
+    { name: 'Page A', uv: 600, pv: 2400, amt: 2800 },
+    { name: 'Page A', uv: 700, pv: 2400, amt: 2900 },
+
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BarChart width={600} height={300} data={data}>
+        <XAxis dataKey="name" stroke="#8884d8" />
+        <YAxis />
+        <Tooltip />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Bar dataKey="uv" fill="#8884d8" barSize={30} />
+      </BarChart>
+
   );
 }
 
